d8dbae85080f5daa53e8efaa22629b7d68d060ca
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-23 19:33:52 +0100

    comparison in java

---
+++ src/com/company/Comparison.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+import java.util. *;
+
+public class Comparison {
+    public static void main(String[] args) {
+
+        //Operators: <, >, <=, >=, &&, ||, !==, !
+        int a, b, c, d;
+        boolean compare;
+
+        a = 2;
+        b = 4;
+        c = 6;
+        d = 8;
+
+        compare = a < b || c > d;
+        System.out.println(compare);
+
+    }
+}
